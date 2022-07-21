1c20bffff585c00b0a632d0eeda49b54f90813e4
Akilesh-programmer <akileshakileshs1234@gmail.com>
2022-07-21 21:02:04 +0530

    add Main.java

---
+++ src/com/akilesh/Main.java
@@ -0,0 +1,12 @@
+package com.akilesh;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // TODO Create book class
+        // TODO CRUD book to database
+        // TODO never ending while loop to access everything
+        // TODO program should not crash with invalid input
+    }
+}
