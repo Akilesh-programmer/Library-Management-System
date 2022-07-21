2e4bbb144238238e36a62de8529aa321bfe05540
Akilesh-programmer <akileshakileshs1234@gmail.com>
2022-07-21 21:43:48 +0530

    wrote code for book class and also created some test cases in Main.py, still need to create some more test cases.

--- src/com/akilesh/Book.java
+++ src/com/akilesh/Book.java
@@ -2,5 +2,136 @@ package com.akilesh;
 
 public class Book {
 
+    // Attributes
+    private String title;
+    private String author;
+    private String publisher;
+    private String type;
+    private int year;
+    private int pages;
+    private double cost;
+    private boolean isIn; // Can track if the book is in library or is with someone else.
+    private String nameOfBookHolder; // Holds the name of the person who is currently having the book.
+    private String phoneNumberOfBookHolder;
+    private int tokenNumberOfBookHolder;
 
+    // Constructor
+    public Book(String title, String author, String publisher, String type, int year, int pages, double cost) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.type = type;
+        this.year = year;
+        this.pages = pages;
+        this.cost = cost;
+        isIn = true;
+        nameOfBookHolder = "";
+        phoneNumberOfBookHolder = "null";
+        tokenNumberOfBookHolder = -1;
+    }
+
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public boolean isIn() {
+        return isIn;
+    }
+
+    public String getNameOfBookHolder() {
+        return nameOfBookHolder;
+    }
+
+    public String getPhoneNumberOfBookHolder() {
+        return phoneNumberOfBookHolder;
+    }
+
+    public int getTokenNumberOfBookHolder() {
+        return tokenNumberOfBookHolder;
+    }
+
+    // Setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public void setCost (double cost) {
+        this.cost = cost;
+    }
+
+    public void setIn(boolean in) {
+        isIn = in;
+    }
+
+    public void setNameOfBookHolder(String nameOfBookHolder) {
+        this.nameOfBookHolder = nameOfBookHolder;
+    }
+
+    public void setPhoneNumberOfBookHolder(String phoneNumberOfBookHolder) {
+        this.phoneNumberOfBookHolder = phoneNumberOfBookHolder;
+    }
+
+    public void setTokenNumberOfBookHolder(int tokenNumberOfBookHolder) {
+        this.tokenNumberOfBookHolder = tokenNumberOfBookHolder;
+    }
+
+    // Functions
+    public void lendBook(String nameOfBookHolder, String phoneNumberOfBookHolder, int tokenNumberOfBookHolder) {
+        this.nameOfBookHolder = nameOfBookHolder;
+        this.phoneNumberOfBookHolder = phoneNumberOfBookHolder;
+        this.tokenNumberOfBookHolder = tokenNumberOfBookHolder;
+        this.isIn= false;
+    }
+
+    public void returnBook() {
+        isIn = true;
+        nameOfBookHolder = "";
+        phoneNumberOfBookHolder = "null";
+        tokenNumberOfBookHolder = -1;
+    }
 }
--- src/com/akilesh/Main.java
+++ src/com/akilesh/Main.java
@@ -5,8 +5,37 @@ public class Main {
     public static void main(String[] args) {
 
         // TODO Create book class
+        // TODO Create class to work with database, it should print everything is asked, and should have all functions
+                // TODO related to CRUD.
         // TODO CRUD book to database
         // TODO never ending while loop to access everything
         // TODO program should not crash with invalid input
+        // TODO create validation for the token number when entered
+
+        // Test Cases for Book class
+        Book book = new Book("Harry Potter", "J K Rowling", "Bloomsbury Publishing",
+                "Fiction", 2002, 250, 10.00);
+        System.out.println("Title : " + book.getTitle());
+        System.out.println("Author : " + book.getAuthor());
+        System.out.println("Publisher : " + book.getPublisher());
+        System.out.println("Type : " + book.getType());
+        System.out.println("Year : " + book.getYear());
+        System.out.println("Pages : " + book.getPages());
+        System.out.println("Cost : " + book.getCost());
+        book.setTitle("Nothing");
+        book.setAuthor("No One");
+        book.setPublisher("No One");
+        book.setType("Nothing");
+        book.setYear(0);
+        book.setPages(0);
+        book.setCost(0);
+        System.out.println("Title : " + book.getTitle());
+        System.out.println("Author : " + book.getAuthor());
+        System.out.println("Publisher : " + book.getPublisher());
+        System.out.println("Type : " + book.getType());
+        System.out.println("Year : " + book.getYear());
+        System.out.println("Pages : " + book.getPages());
+        System.out.println("Cost : " + book.getCost());
+// ---------------------------------------------------------------------------------------------------------------------
     }
 }
